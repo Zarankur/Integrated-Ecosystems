9b1556098efaf52aa377b0c6d659417770885240
Zarankur <pironchristophe92@gmail.com>
2020-02-02 21:16:51 +0100

    adding aframe

---
+++ src/app/aframe/aframe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aframe',
+  templateUrl: './aframe.component.html',
+  styleUrls: ['./aframe.component.scss']
+})
+export class AframeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
